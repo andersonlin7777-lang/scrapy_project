ef22c18628c2e4c0d8e8b31f101ffe49c8b47a4e
anderson <andersonlin7777@gmail.com>
2025-12-25 20:08:47 +0800

    handle error with errorback in book.py

--- books/books/spiders/book.py
+++ books/books/spiders/book.py
@@ -7,6 +7,15 @@ class BookSpider(scrapy.Spider):
     allowed_domains = ["books.toscrape.com"]#限制這隻 spider 只能抓這個網域
     start_urls = ["https://books.toscrape.com/"]
 
+    def start_requests(self):#use .log_error() for the initial request to Books to Scrape
+        for url in self.start_urls:
+            yield scrapy.Request(
+                url, callback=self.parse, errorback=self.log_error
+            )
+
+    def log_error(self, failure):
+        self.logger.error(repr(failure))
+
     def parse(self, response):#response = 抓回來的 HTML 包裝物件
         for book in response.css("article.product_pod"):
             item = BooksItem()
@@ -22,5 +31,9 @@ class BookSpider(scrapy.Spider):
                 f"navigating to next page with URL {next_page_url}"
             )
             #告訴 Scrapy：去抓 next_page_url抓回來後，再交給 parse() 處理一次
-            yield scrapy.Request(url=next_page_url, callback=self.parse)
+            yield scrapy.Request(
+                url=next_page_url,
+                callback=self.parse,
+                errorback=self.log_error
+            )
             
